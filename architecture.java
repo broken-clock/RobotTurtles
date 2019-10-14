08c4a6ff1476b2b89efaa54e4f67ce6f66c5ce95
broken-clock <55990022+broken-clock@users.noreply.github.com>
2019-10-14 12:47:10 +0200

    Create architecture.java

---
+++ architecture.java
@@ -0,0 +1,14 @@
+public static void jouer() {
+    demander nombre de joueurs();
+    initialiser plateau();
+    initialiser compteur de points();
+    initialiserpioches();
+    tours des joueurs();
+        
+}
+
+public static void tours des joueurs() {
+    choix de l'action();
+    placement mur;
+        
+}
